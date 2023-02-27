219940de035d675071cfcde8b54172ec08b81566
iskamele <iskamele@gmail.com>
2023-02-27 16:01:05 +0200

    Refactored code.

--- src/main/java/Tests/CorrectBracketExpression/impl/BracketCombinationsServiceImpl.java
+++ src/main/java/Tests/CorrectBracketExpression/impl/BracketCombinationsServiceImpl.java
@@ -5,26 +5,22 @@ import java.util.List;
 import Tests.CorrectBracketExpression.service.BracketCombinationsService;
 
 public class BracketCombinationsServiceImpl implements BracketCombinationsService {
-
     @Override
     public List<String> getCombinations(int number) {
         List<String> result = new ArrayList<>();
-        char[] str = new char[2 * number];
-        combinations(str, 0, number, 0, 0, result);
+        generateCombinations(result, "", 0, 0, number);
         return result;
     }
 
-    static void combinations(char[] str, int pos, int n, int open, int close, List<String> result) {
-        if (close == n) {
-            result.add(new String(str));
+    private void generateCombinations(List<String> result, String current, int open, int close, int number) {
+        if (current.length() == 2 * number) {
+            result.add(current);
         } else {
-            if (open > close) {
-                str[pos] = ')';
-                combinations(str, pos + 1, n, open, close + 1, result);
+            if (open < number) {
+                generateCombinations(result, current + "(", open + 1, close, number);
             }
-            if (open < n) {
-                str[pos] = '(';
-                combinations(str, pos + 1, n, open + 1, close, result);
+            if (close < open) {
+                generateCombinations(result, current + ")", open, close + 1, number);
             }
         }
     }
