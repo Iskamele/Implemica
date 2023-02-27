791a35b3b841d8931b35b99c953e4e8bf127a78e
iskamele <iskamele@gmail.com>
2023-02-27 14:43:59 +0200

    Added implementation for CatalanNumbersService.

---
+++ src/main/java/Tests/CorrectBracketExpression/impl/CatalanNumbersServiceImpl.java
@@ -0,0 +1,19 @@
+package Tests.CorrectBracketExpression.impl;
+
+import Tests.CorrectBracketExpression.service.CatalanNumbersService;
+
+public class CatalanNumbersServiceImpl implements CatalanNumbersService {
+
+    @Override
+    public int getNumber(int number) {
+        if (number == 0)
+            return 1;
+        else {
+            int count = 0;
+            for (int i = 0; i < number; i++) {
+                count += getNumber(i) * getNumber(number - 1 - i);
+            }
+            return count;
+        }
+    }
+}
