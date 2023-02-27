f2731f657782ce94532b2b5f46e131bde0dbf51d
iskamele <iskamele@gmail.com>
2023-02-27 14:44:07 +0200

    Added implementation for InputService.

---
+++ src/main/java/Tests/CorrectBracketExpression/impl/InputServiceImpl.java
@@ -0,0 +1,19 @@
+package Tests.CorrectBracketExpression.impl;
+
+import java.util.Scanner;
+import Tests.CorrectBracketExpression.service.InputService;
+
+public class InputServiceImpl implements InputService {
+
+    private final Scanner scanner;
+
+    public InputServiceImpl() {
+        scanner = new Scanner(System.in);
+    }
+
+    @Override
+    public int readInt() {
+        System.out.println("Enter number: ");
+        return scanner.nextInt();
+    }
+}
