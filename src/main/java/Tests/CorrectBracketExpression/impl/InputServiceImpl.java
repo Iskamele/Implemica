9d83ec6a0a32dc6e509fe10446cebedfacdc8d9d
iskamele <iskamele@gmail.com>
2023-02-28 11:53:24 +0200

    Added exceptions.

--- src/main/java/Tests/CorrectBracketExpression/impl/InputServiceImpl.java
+++ src/main/java/Tests/CorrectBracketExpression/impl/InputServiceImpl.java
@@ -1,5 +1,6 @@
 package Tests.CorrectBracketExpression.impl;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import Tests.CorrectBracketExpression.service.InputService;
 
@@ -12,7 +13,14 @@ public class InputServiceImpl implements InputService {
 
     @Override
     public int readInt() {
-        System.out.println("Enter number: ");
-        return scanner.nextInt();
+        System.out.println("Enter positive number: ");
+        if (!scanner.hasNextInt()) {
+            throw new InputMismatchException("Incorrect number type.");
+        }
+        int number = scanner.nextInt();
+        if (number <= 0) {
+            throw new RuntimeException("The number must be greater than 0.");
+        }
+        return number;
     }
 }
