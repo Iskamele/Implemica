66e0549fb27b8810f534ba6653a3b2ab43df7475
iskamele <iskamele@gmail.com>
2023-03-05 17:07:37 +0200

    Changed method signature.

--- src/main/java/implemica/FindShortestPath/service/FindShortestPath.java
+++ src/main/java/implemica/FindShortestPath/service/FindShortestPath.java
@@ -1,7 +1,8 @@
 package implemica.FindShortestPath.service;
 
 import implemica.FindShortestPath.model.City;
+import implemica.FindShortestPath.model.Test;
 
 public interface FindShortestPath {
-    int findShortestPath(City from, City to);
+    int findShortestPath(Test test, City from, City to);
 }
