6097cdfa39aa55ed80913c6f1b9bdf32be31d7da
iskamele <iskamele@gmail.com>
2023-03-05 20:22:36 +0200

    Refactored code, also fixed naming.

--- src/main/java/implemica/FindShortestPath/impl/FileParserServiceImpl.java
+++ src/main/java/implemica/FindShortestPath/impl/FileParserServiceImpl.java
@@ -1,73 +1,65 @@
 package implemica.FindShortestPath.impl;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import implemica.FindShortestPath.model.City;
 import implemica.FindShortestPath.model.Test;
 import implemica.FindShortestPath.service.FileParserService;
 
 public class FileParserServiceImpl implements FileParserService {
-    private static final int INDEX_TESTS_COUNT = 0;
-    private static final int INDEX_START_LINE = 1;
-    private int processedLines = INDEX_START_LINE;
+    private static final int TESTS_COUNT_INDEX = 0;
+    private static final int START_LINE_INDEX = 1;
+    private int processedLines = START_LINE_INDEX;
+    private int testIndex = 1;
+    private final Map<Integer, List<String>> neighborsMap = new HashMap<>();
 
     @Override
     public List<Test> parseInput(List<String> input) {
-        List<Test> tests = new ArrayList<>();
-
-        int testsNumber = Integer.parseInt(input.get(INDEX_TESTS_COUNT));
-        // int testIndex = 1;
-        while (testsNumber > 0) {
-            testsNumber--;
-            tests.add(newFun(input));
-        }
-        return tests;
+        int testsNumber = Integer.parseInt(input.get(TESTS_COUNT_INDEX));
+        return IntStream.range(0, testsNumber)
+                .mapToObj(i -> createTest(input))
+                .collect(Collectors.toList());
     }
 
-    private Test newFun(List<String> input) {
+    private Test createTest(List<String> input) {
         Test test = new Test();
-        Map<Integer, List<String>> neighborsMap = new HashMap<>();
-        processedLines = addCities(input, test, neighborsMap, processedLines);
-        addNeighbors(test, neighborsMap);
-        processedLines = addRoutes(input, test, processedLines);
-        //test.setNumber(testIndex);
+        addCities(input, test);
+        addNeighbors(test);
+        addRoutes(input, test);
+        test.setNumber(testIndex++);
         processedLines++;
         return test;
     }
 
-    private int addCities(List<String> input, Test test, Map<Integer, List<String>> neighborsMap, int processedLines) {
+    private void addCities(List<String> input, Test test) {
         int countCity = Integer.parseInt(input.get(processedLines++));
         int cityId = 1;
-        int i = processedLines;
-        while (countCity > 0 && i < input.size()) {
-            String cityName = input.get(i);
-            int currentCityNeighbors = Integer.parseInt(input.get(i + 1));
-            List<String> currentCityRoad = input.subList(i + 2, i + currentCityNeighbors + 2);
+        while (countCity-- > 0 && processedLines < input.size()) {
+            String cityName = input.get(processedLines);
+            int currentCityNeighbors = Integer.parseInt(input.get(processedLines + 1));
+            List<String> currentCityRoad = input.subList(processedLines + 2, processedLines + currentCityNeighbors + 2);
             neighborsMap.put(cityId, currentCityRoad);
             test.addCity(new City(cityId++, cityName));
-            countCity--;
-            i += currentCityNeighbors + 2;
+            processedLines += currentCityNeighbors + 2;
         }
-        return i;
     }
 
-    private void addNeighbors(Test test, Map<Integer, List<String>> neighborsMap) {
+    private void addNeighbors(Test test) {
         for (Map.Entry<Integer, List<String>> entry : neighborsMap.entrySet()) {
             for (String value : entry.getValue()) {
                 String[] cityIdAndCost = value.split(" ");
                 int neighborId = Integer.parseInt(cityIdAndCost[0]);
                 int neighborCost = Integer.parseInt(cityIdAndCost[1]);
                 City neighbor = test.getCityById(neighborId);
-
-                // TODO заменить на getCity
-                test.getCities().get(entry.getKey() - 1).addNeighbor(neighbor, neighborCost);
+                test.getCityById(entry.getKey()).addNeighbor(neighbor, neighborCost);
             }
         }
     }
 
-    private int addRoutes(List<String> input, Test test, int processedLines) {
+    private void addRoutes(List<String> input, Test test) {
         int routesStartLine = processedLines + 1;
         int routesCount = Integer.parseInt(input.get(processedLines));
         for (int i = routesStartLine; i < routesStartLine + routesCount; i++) {
@@ -77,6 +69,5 @@ public class FileParserServiceImpl implements FileParserService {
             test.addRoute(cityFrom, cityTo);
             processedLines++;
         }
-        return processedLines;
     }
 }
