da221d403f924732dd58a26cc1f1d27bbc942206
iskamele <iskamele@gmail.com>
2023-03-05 17:08:06 +0200

    Refactored code.

--- src/main/java/implemica/FindShortestPath/Main.java
+++ src/main/java/implemica/FindShortestPath/Main.java
@@ -4,30 +4,41 @@ import java.util.List;
 import java.util.Map;
 import implemica.FindShortestPath.impl.FileParserServiceImpl;
 import implemica.FindShortestPath.impl.FileReaderServiceImpl;
+import implemica.FindShortestPath.impl.FileWriterServiceImpl;
 import implemica.FindShortestPath.impl.FindShortestPathImpl;
 import implemica.FindShortestPath.model.City;
+import implemica.FindShortestPath.model.Test;
 import implemica.FindShortestPath.service.FileParserService;
 import implemica.FindShortestPath.service.FileReaderService;
+import implemica.FindShortestPath.service.FileWriterService;
 import implemica.FindShortestPath.service.FindShortestPath;
-import implemica.FindShortestPath.storage.CitiesStorage;
-import implemica.FindShortestPath.storage.RoutesStorage;
 
 public class Main {
+    private static final String TO_FILE_PATH =
+            "src/main/java/implemica/FindShortestPath/output/output.txt";
+
     public static void main(String[] args) {
         String filePath = "src/main/java/implemica/FindShortestPath/input/input.txt";
         FileReaderService fileReader = new FileReaderServiceImpl();
         FileParserService fileParser = new FileParserServiceImpl();
         List<String> fileLines = fileReader.readFile(filePath);
-        fileParser.parseInput(fileLines);
-        for (int i = 0; i < CitiesStorage.getCities().size(); i++) {
-            System.out.println(CitiesStorage.getCities().get(i).toString());
-        }
-        for (Map.Entry<City, City> entry : RoutesStorage.getRoutes().entrySet()) {
-            System.out.println(entry.getKey().getName() + "-" + entry.getValue().getName());
-        }
+
+        List<Test> tests = fileParser.parseInput(fileLines);
+
+        StringBuilder report = new StringBuilder();
         FindShortestPath findShortestPath = new FindShortestPathImpl();
-        for (Map.Entry<City, City> entry : RoutesStorage.getRoutes().entrySet()) {
-            System.out.println(findShortestPath.findShortestPath(entry.getKey(), entry.getValue()));
+        for (Test test : tests) {
+            for (Map.Entry<City, City> entry : test.getRoutes().entrySet()) {
+                report.append(entry.getKey().getName());
+                report.append("-");
+                report.append(entry.getValue().getName());
+                report.append(": ");
+                report.append(findShortestPath.findShortestPath(test, entry.getKey(), entry.getValue()));
+                report.append(System.lineSeparator());
+                System.out.println(findShortestPath.findShortestPath(test, entry.getKey(), entry.getValue()));
+            }
         }
+        FileWriterService fileWriterService = new FileWriterServiceImpl();
+        fileWriterService.writeToFile(report.toString(), TO_FILE_PATH);
     }
 }
